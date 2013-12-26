a171fc239e5156506818d0074300e6ff8eeae3e7
Gordon Watts <gordon@gordonandpaula.net>
2013-12-25 22:32:25 -0500

    Check point - turns out we need to move to HttpClient - something that is availible on all platforms, WebRequest, not so much. It is the new shiny.

---
+++ CERNSSOWindowsDesktop/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CERNSSO
+{
+    public static class Utils
+    {
+        /// <summary>
+        /// See if this uri matches with one of the authorization URI's that we
+        /// might expect back from CERN's SSO.
+        /// </summary>
+        /// <param name="u"></param>
+        /// <returns></returns>
+        public static bool IsCERNSSOAuthUri(this Uri u)
+        {
+            return u.Fragment.StartsWith("/adfs/ls/");
+        }
+    }
+}
--- CERNSSOWindowsDesktop/WebAccess.cs
+++ CERNSSOWindowsDesktop/WebAccess.cs
@@ -1,6 +1,7 @@
-﻿
+﻿using System;
 using System.Net;
 using System.Threading.Tasks;
+
 namespace CERNSSO
 {
     /// <summary>
@@ -8,6 +9,11 @@ namespace CERNSSO
     /// </summary>
     public static class WebAccess
     {
+        /// <summary>
+        /// Called to prepare the web request before it is sent out the first time.
+        /// </summary>
+        public static Action<HttpWebRequest> gPrepWebRequest = null;
+
         /// <summary>
         /// After calling this future web accesses will use this username and password
         /// to access web resources. All previous login information will be erased, along
@@ -21,6 +27,12 @@ namespace CERNSSO
         /// </remarks>
         public static void LoadUsernamePassword(string username, string password)
         {
+            // We need to assure full interaction with the ASP.NET web service back-end. So simulate Chrome,
+            // which happens to be the browser we used to debug this interaction.
+            gPrepWebRequest = req =>
+            {
+                req.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36";
+            };
         }
 
         /// <summary>
@@ -37,7 +49,29 @@ namespace CERNSSO
         /// <returns></returns>
         public static async Task<WebResponse> FetchWebResponse(HttpWebRequest request)
         {
-            return await Task.Factory.FromAsync<WebResponse>(request.BeginGetResponse, request.EndGetResponse, null);
+            // Run the web request. Perhaps we will get lucky. We know we are lucky if one
+            // of the authentication guys doesn't come back, or the resource that responds
+            // is the resource we asked for information from.
+
+            PrepWebRequest(request);
+            var response = await Task.Factory.FromAsync<WebResponse>(request.BeginGetResponse, request.EndGetResponse, null);
+            if (response.ResponseUri.OriginalString == request.RequestUri.OriginalString)
+                return response;
+            if (!response.ResponseUri.IsCERNSSOAuthUri())
+                return response;
+
+            throw new NotImplementedException();
         }
+
+        /// <summary>
+        /// Call the prep web request delegate.
+        /// </summary>
+        /// <param name="request"></param>
+        private static void PrepWebRequest(HttpWebRequest request)
+        {
+            if (gPrepWebRequest != null)
+                gPrepWebRequest(request);
+        }
+
     }
 }
