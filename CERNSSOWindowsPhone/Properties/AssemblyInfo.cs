1d75927486863f303e2769815ba3eb8700278465
Gordon Watts <gordon@gordonandpaula.net>
2013-12-26 01:28:27 -0500

    Add common assembly info so that all the libraries are kept in sync.

--- CERNSSOWindowsPhone/Properties/AssemblyInfo.cs
+++ CERNSSOWindowsPhone/Properties/AssemblyInfo.cs
@@ -1,19 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CERNSSOWindowsPhone")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CERNSSOWindowsPhone")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,16 +13,3 @@ using System.Resources;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6ce3dd5c-46d5-49be-b6b3-52e28fbc9fba")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
---
+++ CERNSSOWindowsStore/CommonAssemblyProperties.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+// All the common assembly information
+[assembly: AssemblyDescription("A simple library to aid in authentication to CERN's SSO and give you access to protected websites (with the proper password or cert) easily.")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Gordon Watts")]
+[assembly: AssemblyProduct("CERNSSO")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
