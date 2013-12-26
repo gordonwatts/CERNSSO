b45fae9cbcb71f33ce7ca356d432737599903243
Gordon Watts <gordon@gordonandpaula.net>
2013-12-25 18:34:00 -0600

    First verson of three libraries and test projects. All compile, and the simple tests all pass now.

---
+++ CERNSSOWindowsDesktop/WebAccess.cs
@@ -0,0 +1,31 @@
+﻿
+namespace CERNSSO
+{
+    /// <summary>
+    /// Static methods to enable web requests to CERN protected sites.
+    /// </summary>
+    public static class WebAccess
+    {
+        /// <summary>
+        /// After calling this future web accesses will use this username and password
+        /// to access web resources. All previous login information will be erased, along
+        /// with pre-authenticated sites.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <remarks>
+        /// The current version of the library can't access two different web sites with different
+        /// login credentials.
+        /// </remarks>
+        public static void LoadUsernamePassword(string username, string password)
+        {
+        }
+
+        /// <summary>
+        /// Clears out all information we've cached. This is useful mainly for testing
+        /// to make sure the state of the library is reset.
+        /// </summary>
+        public static void ResetCredentials()
+        { }
+    }
+}
---
+++ CERNSSOWindowsDesktopTest/CERNSSOPublic.cs
@@ -0,0 +1,36 @@
+﻿using CERNSSO;
+
+// Get the unit testing framework properly slotted in.
+#if CMPWindowsStore || WINDOWS_PHONE
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+#else
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+
+namespace CERNSSOWindowsDesktopTest
+{
+    /// <summary>
+    /// Test access to public CERN web documents and items
+    /// </summary>
+    [TestClass]
+    public class CERNSSOPublic
+    {
+        /// <summary>
+        /// Reset the library before we test it each time.
+        /// </summary>
+        [TestInitialize]
+        public void TestInit()
+        {
+            WebAccess.ResetCredentials();
+        }
+
+        /// <summary>
+        /// Access a CDS record that is publicly available, without
+        /// having any sort of credentials present.
+        /// </summary>
+        [TestMethod]
+        public void TestPublicCDSRecord()
+        {
+        }
+    }
+}
