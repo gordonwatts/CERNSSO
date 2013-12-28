c9e6fef441a832ceb716879659edcb69003e9b5c
Gordon Watts <gordon@gordonandpaula.net>
2013-12-28 01:32:34 -0500

    Added a PCL version of this library.

---
+++ CERNSSOPCL/CERNWebAccess.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+namespace CERNSSOPCL
+{
+    /// <summary>
+    /// Uniform Portable Class Library interface to CERN's Single Sign On.
+    /// Username/Password is cross-platform, but use of a certificate isn't. You'll
+    /// have to use a specific version of the platform library to effect that.
+    /// </summary>
+    public class CERNWebAccess
+    {
+        /// <summary>
+        /// Use a username and password to log onto the CERN SSO. This is cached for
+        /// the duration of the code (it isn't cached anywhere).
+        /// </summary>
+        /// <param name="username">CERN Username</param>
+        /// <param name="password">CERN Password</param>
+        public static void LoadUsernamePassword(string username, string password)
+        {
+#if WINDOWS_DESKTOP || WINDOWS_PHONE || CMPWindowsStore
+            CERNSSO.WebAccess.LoadUsernamePassword(username, password);
+#else
+            throw new NotImplementedException("The CERNSSO Platform Specific Library has not been included in this project! This is a coding and deployment error.");
+#endif
+        }
+
+        /// <summary>
+        /// We are given a prepared HTTP web request. Fetch a working response.
+        /// </summary>
+        /// <param name="requestUri">The URI of the CERN SSO resource you want to fetch.</param>
+        /// <returns>A response object that should have the data requested in it. Or if authentication fails, it will throw an UnauthorizedAccess exception.</returns>
+        /// <remarks>If this is called against a URI that doesn't require authentication, this should work just fine.</remarks>
+        public static Task<HttpResponseMessage> GetWebResponse(Uri requestUri)
+        {
+#if WINDOWS_DESKTOP || WINDOWS_PHONE || CMPWindowsStore
+            return CERNSSO.WebAccess.GetWebResponse(requestUri);
+#else
+            throw new NotImplementedException("The CERNSSO Platform Specific Library has not been included in this project! This is a coding and deployment error.");
+#endif
+        }
+
+        /// <summary>
+        /// Clears out all information we've cached.
+        /// </summary>
+        /// <remarks>This was added to aid in testing, mostly.</remarks>
+        public static void ResetCredentials()
+        {
+#if WINDOWS_DESKTOP || WINDOWS_PHONE || CMPWindowsStore
+            CERNSSO.WebAccess.ResetCredentials();
+#else
+            throw new NotImplementedException("The CERNSSO Platform Specific Library has not been included in this project! This is a coding and deployment error.");
+#endif
+        }
+    }
+}
--- readme.txt
+++ readme.txt
@@ -5,4 +5,9 @@ Windows Phone 8 coding? There is an issue!
 HtmlAgilityPack doesn't properly install.
 You'll have to manually add the HtmlAgilityPack.dll to your project.
 Do this by adding it from the sl3-wp folder in the packages directory of
-your solution (for standard nuget usage) Thanks!
+your solution (for standard NuGet usage) Thanks!
+
+If you are referencing this in a Portable Class Library (PCL) then you *must* reference
+it in the platform specific library that calls your PCL. If you do not you'll
+get an exception complaining that the platform specific version of the library has not
+been referenced.
