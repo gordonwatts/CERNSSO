5bb145407d7483cc440d1b4071b66355e3a6bbcc
Gordon Watts <gordon@gordonandpaula.net>
2015-01-14 02:45:27 +0100

    Update the version info

--- CERNSSO/Properties/AssemblyInfo.cs
+++ CERNSSO/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CERNSSO")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Eases access to the CERN SSO websites given a good CERN certificate")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CERNSSO")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
